e1b28614793f6a497b1fe35339d917d5ec5866ef
rhit-yangh6 <yangh6@rose-hulman.edu>
2021-02-06 01:17:11 +0800

    Initial Commit

---
+++ 64Collection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  64Collection
+//
+//  Created by Hanyu Yang on 2021/2/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
