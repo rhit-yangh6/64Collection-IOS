ccb186114212d8ce4f1618d8e86993848ba8bc7f
rhit-yangh6 <yangh6@rose-hulman.edu>
2021-05-20 00:14:31 +0800

    test push from AppCode

--- 64Collection/View Controllers/TypeTableViewController.swift
+++ 64Collection/View Controllers/TypeTableViewController.swift
@@ -8,38 +8,38 @@
 import UIKit
 
 class TypeTableViewController: UITableViewController, UISearchBarDelegate {
-    
+
     var brandId: String!
     @IBOutlet weak var typeSearchBar: UISearchBar!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.typeSearchBar.delegate = self
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh,
-                                                            target: self,
-                                                            action: #selector(refresh))
-        
+                target: self,
+                action: #selector(refresh))
+
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         refresh()
     }
-    
+
     @objc func refresh() {
         BackendService.shared.retrieveTypesList(searchString: self.typeSearchBar.text ?? "",
-                                                brandId: self.brandId,
-                                                changeListener: self.tableView.reloadData)
+                brandId: self.brandId,
+                changeListener: self.tableView.reloadData)
         //        LeanCloudService.shared.retrieveTypesList(searchString: self.typeSearchBar.text ?? "",
         //                                                  brandId: self.brandId,
         //                                                  changeListener: self.tableView.reloadData)
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         BackendService.shared.getTypesCount()
         // return LeanCloudService.shared.getTypesCount()
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: typeCellIdentifier, for: indexPath) as! TypeCell
         let typeDto = BackendService.shared.getTypeAtIndex(index: indexPath.row)
@@ -50,29 +50,31 @@ class TypeTableViewController: UITableViewController, UISearchBarDelegate {
         // print(typeDto.imgUrls)
         return cell
     }
-    
+
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return false
     }
-    
+
     //    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
     //        if editingStyle == .delete {
     //            TypesManager.shared.deleteTypeWithId(id: TypesManager.shared.getTypeIdAtIndex(index: indexPath.row), changeListener: refresh)
     //        }
     //    }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == typeDetailSegueIdentifier {
             if let indexPath = tableView.indexPathForSelectedRow {
                 (segue.destination as! DetailViewController).typeDto =
-                    BackendService.shared.getTypeAtIndex(index: indexPath.row)
+                        BackendService.shared.getTypeAtIndex(index: indexPath.row)
                 // LeanCloudService.shared.getTypeAtIndex(index: indexPath.row)
             }
         }
     }
-    
-    public func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) { self.refresh() }
-    
+
+    public func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.refresh()
+    }
+
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
     }
